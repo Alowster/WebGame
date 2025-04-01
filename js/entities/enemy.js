691e5e80208c7e5d14011e4d5b556eee9605f16b
Alowster <viallo@campusaula.com>
2025-04-01 18:51:54 +0200

    bu

--- js/entities/enemy.js
+++ js/entities/enemy.js
@@ -1,17 +1,26 @@
-const enemyVelocity = 0.6;
+export class Enemy {
+    constructor() {
+        
+        this.pantalla = document.getElementById('pantalla');
+        this.castillo = document.getElementById('castillo');
+        this.explosion = document.getElementById('explosion');
 
-const pantalla = document.getElementById('pantalla');
-const castillo = document.getElementById('castillo');
-const explosion = document.getElementById('explosion');
+        this.enemy = document.createElement('div');
 
-function createEnemy() {
-    console.log('Creando enemigo...');
-    const enemy = document.createElement('div');
-    let enemySide = randomEnemy(enemy);
+        this.enemy.style.position = 'absolute';
+        this.enemy.style.top = '83%';
+        this.enemy.style.width = '50px';
+        this.enemy.style.height = '50px';
 
-    pantalla.appendChild(enemy);
+        this.enemySide = this.randomEnemy(enemy);
+    
+        this.pantalla.appendChild(enemy);
 
-    function moveEnemy() {
+        this.moveEnemy(enemy, 0.6);
+
+    }
+
+    moveEnemy(enemy, enemyVelocity) {
         let enemyPos = parseFloat(getComputedStyle(enemy).left);
 
         if (enemySide === 1) {
@@ -27,62 +36,54 @@ function createEnemy() {
             requestAnimationFrame(moveEnemy);
         }
 
-
-    }
-    requestAnimationFrame(moveEnemy);
-}
-
-function randomEnemy(enemy) {
-    var caseNum = numeroAleatorio();
-    let enemySide;
-
-    switch (caseNum) {
-        case 1:
-        case 3:
-            enemy.classList.add('enemyLeft','enemigo');
-            enemy.style.left = '0px';
-            enemySide = 1;
-            break;
-        case 2:
-        case 4:
-            enemy.classList.add('enemyRight','enemigo');
-            enemy.style.left = '97%';
-            enemySide = 2;
-            break;
     }
 
-    enemy.style.position = 'absolute';
-    enemy.style.top = '83%';
-    enemy.style.width = '50px';
-    enemy.style.height = '50px';
-
-    switch (caseNum) {
-        case 1:
-        case 3:
-            enemy.style.backgroundColor = 'red';
-            break;
-        case 2:
-        case 4:
-            enemy.style.backgroundColor = 'blue';
-            break;
+    randomEnemy(enemy) {
+        var caseNum = this.numeroAleatorio();
+        let enemySide;
+    
+        switch (caseNum) {
+            case 1:
+            case 3:
+                enemy.classList.add('enemyLeft','enemigo');
+                enemy.style.left = '0px';
+                enemySide = 1;
+                break;
+            case 2:
+            case 4:
+                enemy.classList.add('enemyRight','enemigo');
+                enemy.style.left = '97%';
+                enemySide = 2;
+                break;
+        }
+    
+        switch (caseNum) {
+            case 1:
+            case 3:
+                enemy.style.backgroundColor = 'red';
+                break;
+            case 2:
+            case 4:
+                enemy.style.backgroundColor = 'blue';
+                break;
+        }
+    
+        return enemySide;
     }
 
-    return enemySide;
-}
+    numeroAleatorio() {
+        return Math.floor(Math.random() * 4) + 1;
+    }
 
-function numeroAleatorio() {
-    return Math.floor(Math.random() * 4) + 1;
+    checkCollision(enemy, castillo) {
+        let rect1 = enemy.getBoundingClientRect();
+        let rect2 = castillo.getBoundingClientRect();
+        return (
+            rect1.left < rect2.right &&
+            rect1.right > rect2.left &&
+            rect1.top < rect2.bottom &&
+            rect1.bottom > rect2.top
+        );
+    }
+    
 }
-
-function checkCollision(enemy, castillo) {
-    let rect1 = enemy.getBoundingClientRect();
-    let rect2 = castillo.getBoundingClientRect();
-    return (
-        rect1.left < rect2.right &&
-        rect1.right > rect2.left &&
-        rect1.top < rect2.bottom &&
-        rect1.bottom > rect2.top
-    );
-}
-
-setInterval(createEnemy, 900); // Crea un enemigo cada 2 segundos
